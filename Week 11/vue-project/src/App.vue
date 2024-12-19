72733e841e2f6a331e7e3d85b553f7c1abf88df8
Jesslyn Caroline <158556535+jesslyn-caroline@users.noreply.github.com>
2024-12-19 07:41:44 +0000

    Update

---
+++ Week 11/vue-project/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <h1>{{ message }}</h1>
+  <food-item/>
+  <food-item/>
+  <food-item/>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: 'This is some text'
+    };
+  }
+};
+</script>
+
+<style>
+
+  #app > div {
+    border: dashed black 1px;
+    display: inline-block;
+    margin: 10px;
+    padding: 10px;
+    background-color: lightgreen;
+  }
+
+</style>
